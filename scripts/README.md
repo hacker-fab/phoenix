88824be8e2a408af3f546feba6c122d2c7ccf5b7
Waterloo Hacker Fab <waterloofab@hackerfabmacmini.uwaterloo.ca>
2025-09-09 21:41:20 -0400

    found docs on Co and RS

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+how to run example script
+
+```
+
+```
--- scripts/lcd.py
+++ scripts/lcd.py
@@ -1,12 +1,29 @@
 from machine import I2C, Pin
 import time
+from enum import Enum
 
 LCD_RESET = 14
 
-I2C_ADDR = 0x78
-COM = 0x00
+I2C_ADDR = 60
+CONTROL = 0x00
 DATA = 0x00
 LINE2 = 0xC0
+
+class CtrlContinue(Enum):
+    LAST_CTRL = 0
+    MORE_CTRL = 1
+
+class CtrlType(Enum):
+    INSTRUCTION = 0
+    DATA = 1
+
+class ControlByte():
+    def __init__(self, co, rs):
+        self.co = co 
+        self.rs = rs
+    def to_byte(self):
+        return (self.co.value << 7) | (self.rs.value << 6)
+
 
 lcd_reset = Pin(LCD_RESET, Pin.OUT)
 
@@ -20,10 +37,17 @@ i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=250_000)
 print(i2c.scan())
 
 # def write_data(data):
-#     i2c.writeto(I2C_ADDR, bytearray([DATA, data]))
+#     stuff = bytearray([my_ctrl, data])
+#     print(stuff)
+#     i2c.writeto(I2C_ADDR, stuff)
 
-text = "hello"
-print(bytearray([DATA, text[0]]))
-for c in text.encode('ascii')[:20]:
-    print(bytearray([DATA, c]))
-    # write_data(c)
+# write data
+my_ctrl = ControlByte(co=CtrlContinue.MORE_CTRL, rs=CtrlType.DATA)
+
+text = "HELLO"
+data = text.encode('ascii')[:20]
+
+sequence = bytearray([my_ctrl, data])
+i2c.writeto(I2C_ADDR, sequence)
+
+print("write done")
